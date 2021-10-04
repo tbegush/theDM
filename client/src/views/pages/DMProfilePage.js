848e0d46989fdbcf9157a677820688cd2adf5841
JazmyneB <bradleyj1@email.arizona.edu>
2021-10-03 20:13:16 -0700

    resolving

---
+++ client/src/views/pages/DMProfilePage.js
@@ -0,0 +1,212 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import classnames from "classnames";
+// javascript plugin used to create scrollbars on windows
+import PerfectScrollbar from "perfect-scrollbar";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Label,
+  FormGroup,
+  Form,
+  Input,
+  FormText,
+  NavItem,
+  NavLink,
+  Nav,
+  Table,
+  TabContent,
+  TabPane,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+  UncontrolledCarousel,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/DMNavbar.js";
+import Footer from "components/Footer/DMFooter.js";
+
+// DM components
+import UsersList from "components/DM/UsersList";
+
+const carouselItems = [
+  {
+    src: require("assets/img/denys.jpg").default,
+    altText: "Slide 1",
+    caption: "Big City Life, United States",
+  },
+  {
+    src: require("assets/img/fabien-bazanegue.jpg").default,
+    altText: "Slide 2",
+    caption: "Somewhere Beyond, United States",
+  },
+  {
+    src: require("assets/img/mark-finn.jpg").default,
+    altText: "Slide 3",
+    caption: "Stocks, United States",
+  },
+];
+
+let ps = null;
+
+export default function DMProfilePage() {
+  const [tabs, setTabs] = React.useState(1);
+  React.useEffect(() => {
+    if (navigator.platform.indexOf("Win") > -1) {
+      document.documentElement.className += " perfect-scrollbar-on";
+      document.documentElement.classList.remove("perfect-scrollbar-off");
+      let tables = document.querySelectorAll(".table-responsive");
+      for (let i = 0; i < tables.length; i++) {
+        ps = new PerfectScrollbar(tables[i]);
+      }
+    }
+    document.body.classList.toggle("profile-page");
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      if (navigator.platform.indexOf("Win") > -1) {
+        ps.destroy();
+        document.documentElement.className += " perfect-scrollbar-off";
+        document.documentElement.classList.remove("perfect-scrollbar-on");
+      }
+      document.body.classList.toggle("profile-page");
+    };
+  },[]);
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <div className="page-header">
+            <img
+              alt="..."
+              className="dots"
+              src={require("assets/img/dots.png").default}
+            />
+            <img
+              alt="..."
+              className="path"
+              src={require("assets/img/path4.png").default}
+            />
+        </div>
+        <section className="section section-lg">
+          <section className="section">
+          <Container className="align-items-center">
+            <Row>
+              <Col className="ml-auto mr-auto" lg="12" md="12">
+                <Card className="card-coin card-plain">
+                  <CardHeader>
+                    <img
+                      alt="..."
+                      className="img-center img-fluid rounded-circle"
+                      src={require("assets/img/mike.jpg").default}
+                    />
+                    <h4 className="title">(USERNAME)</h4>
+                  </CardHeader>
+                  <CardBody>
+                    <Nav
+                      className="nav-tabs-primary justify-content-center"
+                      tabs
+                    >
+                      <NavItem>
+                        <NavLink
+                          className={classnames({
+                            active: tabs === 1,
+                          })}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setTabs(1);
+                          }}
+                          href="#pablo"
+                        >
+                          Messages
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          className={classnames({
+                            active: tabs === 2,
+                          })}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setTabs(2);
+                          }}
+                          href="#pablo"
+                        >
+                          Search
+                        </NavLink>
+                      </NavItem>
+                    </Nav>
+                    <TabContent
+                      className="tab-subcategories"
+                      activeTab={"tab" + tabs}
+                    >
+                    <TabPane tabId="tab1">
+                      <Row>
+                        <Label sm="3">Search Users</Label>
+                        <Col sm="9">
+                            <FormGroup>
+                              <Input
+                                placeholder="Ashley Zeel"
+                                type="text"
+                              />
+                            </FormGroup>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Label sm="3"></Label>
+                        <Col sm="9">
+                        </Col>
+                      </Row>
+                    </TabPane>
+                      <TabPane tabId="tab2">
+                        <Row>
+                          <Label sm="3">Search Users</Label>
+                          <Col sm="9">
+                            <FormGroup>
+                              <Input
+                                placeholder="Steve Long"
+                                type="text"
+                              />
+                            </FormGroup>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Label sm="3"></Label>
+                          <Col sm="9">
+                          </Col>
+                        </Row>
+                        <UsersList></UsersList>
+                      </TabPane>
+                    </TabContent>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        </section>
+        <Footer />
+      </div>
+    </>
+  );
+}
