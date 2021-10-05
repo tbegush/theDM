742ced98f3d2938ac59cc159fdaa0b7332dc6e98
Thomas J Begush <tbegush@gmail.com>
2021-10-05 18:29:48 -0400

    updated server.js path from absolute to relative

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
   }
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
   
 
