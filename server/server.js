33f3cd5d42e62cec8fb55ba47aaf65da37772e3c
Thomas J Begush <tbegush@gmail.com>
2021-10-05 18:42:36 -0400

    trying to get heroku running

--- server/server.js
+++ server/server.js
@@ -33,11 +33,11 @@ app.use('/images', express.static(path.join(__dirname, '../client/src/assets/img
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')));
+    app.use(express.static(path.join(__dirname, '/build')));
   }
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build/index.html'));
   });
   
 
