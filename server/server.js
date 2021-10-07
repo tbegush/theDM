eb03b06d6f681822cd86e636794cf9beb128e67d
Thomas J Begush <tbegush@gmail.com>
2021-10-06 20:31:24 -0400

    fixing heroku deployment

--- server/server.js
+++ server/server.js
@@ -33,12 +33,19 @@ app.use('/images', express.static(path.join(__dirname, '../client/src/assets/img
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')));
-  }
+      app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}   
   
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-  });
+  //   app.use(express.static(path.join(__dirname, '../build')));
+  // }
+  
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../build/index.html'));
+  // });
   
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/thedm', {
